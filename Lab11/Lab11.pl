20c383a2f8cd3ec0f20e54d379f4eeff6347b936
Artur Manukyan <nowrim@mail.ru>
2022-05-06 16:47:49 +0300

    пытаюсь загрузить

---
+++ Lab11/Lab11.pl
@@ -0,0 +1,59 @@
+man(voeneg).
+man(ratibor).
+man(boguslav).
+man(velerad).
+man(duhovlad).
+man(svyatoslav).
+man(dobrozhir).
+man(bogomil).
+man(zlatomir).
+
+woman(goluba).
+woman(lubomila).
+woman(bratislava).
+woman(veslava).
+woman(zhdana).
+woman(bozhedara).
+woman(broneslava).
+woman(veselina).
+woman(zdislava).
+
+parent(voeneg,ratibor).
+parent(voeneg,bratislava).
+parent(voeneg,velerad).
+parent(voeneg,zhdana).
+
+parent(goluba,ratibor).
+parent(goluba,bratislava).
+parent(goluba,velerad).
+parent(goluba,zhdana).
+
+parent(ratibor,svyatoslav).
+parent(ratibor,dobrozhir).
+parent(lubomila,svyatoslav).
+parent(lubomila,dobrozhir).
+
+parent(boguslav,bogomil).
+parent(boguslav,bozhedara).
+parent(bratislava,bogomil).
+parent(bratislava,bozhedara).
+
+parent(velerad,broneslava).
+parent(velerad,veselina).
+parent(veslava,broneslava).
+parent(veslava,veselina).
+
+parent(duhovlad,zdislava).
+parent(duhovlad,zlatomir).
+parent(zhdana,zdislava).
+parent(zhdana,zlatomir).
+
+%11.1 Является ли Х сыном Y.
+son(X,Y):- man(X), parent(Y, X).
+%11.2 Вывести сына Х
+son(X):- man(Y), parent(X, Y), write(Y),nl, fail.
+
+%12.1 Является ли Х сестрой Y.
+sister(X, Y):- woman(X), parent(Z, X), parent(Z, Y).
+%12.2 Вывести всех сестёр Х.
+sisters(X):-woman(Y), woman(Z), parent(Z, X), parent(Z, Y), write(Y), nl, fail.
