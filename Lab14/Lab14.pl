361edb21b0a86a2d7bc3394eccbe8b6e4b71414f
Artur Manukyan <nowrim@mail.ru>
2022-05-09 15:53:33 +0300

    выполнена задача 1.2

--- Lab14/Lab14.pl
+++ Lab14/Lab14.pl
@@ -12,3 +12,31 @@ write_str([H|T]):-put(H), write_str(T).
 %ство символов в ней.
 
 f11:-read_str(S, N), write_str(S), write(', '), write_str(S), write(', '), write_str(S), nl,write('length - '), write(N).
+
+%1.2 Дана строка. Найти количество слов.
+% 4 аргумент(флаг): 1 - слово началось и ищем конец, 0 - пропускаем
+% пробелы.
+words_count(S, C):-words_count(S, 0, C, 0).
+words_count([],C,C,_):-!.
+words_count([32|T], CurC, C, 0):- words_count(T, CurC, C, 0).
+words_count([_|T], CurC, C, 0):- CurC1 is CurC+1, words_count(T, CurC1, C, 1).
+words_count([_|T], CurC, C, 1):- words_count(T, CurC, C, 0).
+
+
+%words_count([],C,C):-!.
+
+f12:-read_str(A,_), countwords(A, K),nl,write(K).
+
+countwords(A,C):-countwords(A,0,C).
+countwords([],K,K):-!.
+countwords(Str,I,K):-skipspace(Str,Snew),getword(Snew,Word,Snew1),Word \=[],I1 is I+1,countwords(Snew1,I1,K),!.
+countwords(_,K,K).
+
+skipspace([32|T],A1):-skipspace(T,A1),!.
+skipspace(A1,A1).
+
+getword([],[],[]):-!.
+getword(A,Word,A2):-getword(A,[],Word,A2).
+getword([],Word,Word,[]).
+getword([32|T],Word,Word,T):-!.
+getword([H|T],W,Word,A2):-append(W,[H],W1),getword(T,W1,Word,A2).
